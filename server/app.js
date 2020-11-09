780b18fd2607183a2638dcb66fb02b9e09c8527d
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-09 11:09:54 +0530

    feat: passport session setup.

--- server/app.js
+++ server/app.js
@@ -32,3 +32,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(session({
+    secret: 'Den Ninja',
+    resave: true,
+    saveUninitialized: true
+}));
